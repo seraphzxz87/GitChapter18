b6756dadca5189d0624126c956bb2792cae2929c
seraphzxz <seraphzxz@sina.com>
2017-07-17 12:02:29 +0800

    init Button

--- app/src/main/java/com/seraphzxz/chapter18_github/MainActivity.java
+++ app/src/main/java/com/seraphzxz/chapter18_github/MainActivity.java
@@ -2,8 +2,11 @@ package com.seraphzxz.chapter18_github;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
